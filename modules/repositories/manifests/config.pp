0efb79bf57ed7d20e683fabf64790a229334234e
root <root@puppet-master.apopx.net>
2017-05-30 02:41:21 +0100

    h

--- modules/repositories/manifests/config.pp
+++ modules/repositories/manifests/config.pp
@@ -1,5 +1,28 @@
 class repositories::config {
 
+case $operatingsystem {
+	"Debian": {include repos::debian}
+}
+
+ if $lsbdistcodename == ""
+        {
+                fail("lsbdistcodename missing, check facter")
+        }
+        else {
+                if $lsbdistcodename == "n/a" and $lsbmajdistrelease == "6" {
+                        # squeeze had a broken lsb-release tool:
+                        # bens@spark:~$ lsb_release  -a
+                        # No LSB modules are available.
+                        # Distributor ID:       Debian
+                        # Description:          Debian GNU/Linux 6.0.4 (n/a)
+                        # Release:                      6.0.4
+                        # Codename:                     n/a
+                        $debianver = "squeeze"
+                } else {
+                        $debianver = $lsbdistcodename
+                }
+        }
+
 file { '/etc/apt/sources.list':
 ensure => present,
 source => 'puppet:///modules/repositories/sources.list',
