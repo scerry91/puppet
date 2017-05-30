1622fe9d7269d139dfb9e708d3ec94a146a42887
root <root@puppet-master.apopx.net>
2017-05-30 23:13:56 +0100

    H

--- modules/repositories/manifests/config.pp
+++ modules/repositories/manifests/config.pp
@@ -1,11 +1,11 @@
 class repositories::config {
-file { '/etc/apt/install':
+file { '/etc/apt/source.list':
 mode => '0755',
 ensure => present,
-source => 'puppet:///modules/repositories/install',
-notify => Exec['install'],
-}
-exec { 'install':
-command => '/etc/apt/install',
+source => 'puppet:///modules/repositories/source.list',
+#notify => Exec['install'],
+#}
+#exec { 'install':
+#command => '/etc/apt/install',
 }
 }
