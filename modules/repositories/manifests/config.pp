a6411650f59fa0b79a6732bc19fb584cf247e5a7
root <root@puppet-master.apopx.net>
2017-05-30 05:49:58 +0100

    hi

--- modules/repositories/manifests/config.pp
+++ modules/repositories/manifests/config.pp
@@ -3,5 +3,8 @@ file { '/etc/apt/install':
 mode => '0755',
 ensure => present,
 source => 'puppet:///modules/repositories/install',
+notify => Exec['install'],
 }
+exec { 'install':
+  command => "/bin/bash -c '/etc/apt/install'",
 }
